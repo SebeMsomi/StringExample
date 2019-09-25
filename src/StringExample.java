67e23bbe0d124b535141b6671e0aa4bd0040df76
Clayton <cmpetersen@hotmail.co.za>
2019-09-25 08:13:44 +0200

    Initial commit

---
+++ src/StringExample.java
@@ -0,0 +1,16 @@
+public class StringExample {
+
+    public static void main(String[] args) {
+
+        String str = "Sample String";
+
+        displayString(str);
+
+    }//end main
+
+    public static void displayString(String str) {
+
+
+    }//end displayString
+
+}//end class
